ae877d801ff3ba646182abce27bbfeaa902f2949
eureka <eureka@eurekanoMacBook-Air.local>
2017-09-07 12:35:46 +0900

    add erb

--- app.rb
+++ app.rb
@@ -6,3 +6,31 @@ require 'sinatra/reloader'
 	get '/' do
 		erb :index 
 	end
+
+	get '/login'
+		erb :login
+	end
+
+	get '/register'
+		erb :register
+	end
+
+	get '/user/follower'
+		erb :follower
+	end
+
+	get '/user/follow'
+		erb :follow
+	end
+
+	get '/tweet'
+		erb :tweet
+	end
+
+	get '/search'
+		erb :search
+	end
+
+	get '/user'
+		erb :user
+	end
